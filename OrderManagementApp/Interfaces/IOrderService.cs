be374b60ba22024db82c806fdf1ccfc0169f16b1
Arkadiusz Kiszka <00oookiocz@gmail.com>
2024-11-08 01:52:58 +0100

    [OrderTask-001] Add Models and Interfaces

---
+++ OrderManagementApp/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using OrderManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementApp.Interfaces
+{
+    public interface IOrderService
+    {
+        void AddOrderItem(int productId, int quantity);
+        void RemoveOrderItem(int productId);
+    }
+}
---
+++ OrderManagementApp/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
